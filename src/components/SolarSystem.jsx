56f0a695559cca96167611fd0d9061da2e8a7b09
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-02-18 15:21:37 -0300

    Requisito 2 - realizado

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        oi
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
